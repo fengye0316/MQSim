50e53a99038c3e7a9cf1012b174ecf6f24f06c84
career.lee <lichuanye0316@126.com>
2019-11-26 22:42:35 -0800

    add info to log object allocation count

---
+++ src/host/Host_IO_Request.cpp
@@ -0,0 +1,6 @@
+#include "Host_IO_Request.h"
+
+namespace Host_Components
+{
+	unsigned int Host_IO_Request::objCount = 0;
+}
---
+++ src/host/PCIe_Message.cpp
@@ -0,0 +1,18 @@
+#include "../sim/Sim_Defs.h"
+#include "PCIe_Message.h"
+namespace Host_Components
+{
+
+	unsigned int PCIe_Message::objCount = 0;
+
+	PCIe_Message::PCIe_Message()
+	{
+		DEBUG_OBJ_ALLOC(typeid(*this).name(), objCount, OBJ_MOD_DEFAULT);
+	}
+
+	PCIe_Message::~PCIe_Message()
+	{
+		DEBUG_OBJ_DELOC(typeid(*this).name(), objCount, OBJ_MOD_DEFAULT);
+	}
+
+}
